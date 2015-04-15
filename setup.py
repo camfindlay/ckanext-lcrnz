30c7b65f9200714e221fdf1994a3d27edbd22f73
amercader <amercadero@gmail.com>
2015-04-15 10:33:34 +0100

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.1'
+version = '0.0.1'
 
 setup(
     name='ckanext-lcrnz',
     version=version,
-    description="",
+    description='CKAN extension for the Landcare Research Datastore',
     long_description='''
     ''',
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
     keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
+    author='Open Knowledge',
+    author_email='services@okfn.org',
+    url='https://github.com/okfn/ckanext-esdstandards',
+    license='AGPL',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.lcrnz'],
     include_package_data=True,
